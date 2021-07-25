921e9a4b33a93e969bd344ca0157d113ec522fdb
Carlos Cangiano <carlos.cangiano@ross.com.ar>
2021-07-25 20:50:38 -0300

    Step 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { default as React, useState, useEffect } from "react";
-
+import './app.css';
 interface EthCurrency {
   type: string,
   price: number,
@@ -36,7 +36,7 @@ function App() {
       price,
       floor,
       decimal: data.close.split('.')[1],
-      timeStamp: data.startTime.second
+      timeStamp: new Date(data.startTime.seconds * 1000).toISOString()
     };    
   };
 
@@ -55,13 +55,14 @@ function App() {
           <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="max-w-4xl mx-auto">
               <dl className="w-1/3 mx-auto bg-white rounded-lg shadow-lg">
-                <div className="flex flex-col p-6 text-center border-t border-gray-100">
+                <div className="flex flex-col p-6 text-center border-t border-gray-100 tooltip">
                   <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                     ETH/USD
                   </dt>
                   <dd className="order-1 text-5xl font-extrabold text-gray-500">
                     ${ethUsd.floor}<span className="text-2xl">.{ethUsd.decimal || '00'}</span>
                   </dd>
+                  <span className="tooltiptext">{ethUsd.timeStamp}</span>
                 </div>
               </dl>
             </div>
